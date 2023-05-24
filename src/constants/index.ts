ba7e9bd91114775ec485ee671b092c75deb9db73
Frans Gustavson Påsse <frans.gop@hotmail.com>
2023-05-25 00:09:46 +0200

    Added projects as an array with both links and images and rendered on projects page

---
+++ src/constants/index.ts
@@ -0,0 +1,46 @@
+import type { Card } from "../types";
+
+export const projects: Card[] = [
+  {
+    img: "../assets/AI_storybook.jpg",
+    title: "AI storybook",
+    body: "My thesis project created with Open AI's Dall-E API, Vue 3, TypeScript and lots of vanilla CSS.",
+    href: "https://ai-storybook.netlify.app/",
+  },
+  {
+    img: "../assets/Malmö_Beermap.jpg",
+    title: "Malmö Beermap",
+    body: "A project about...",
+    href: "https://beermap.netlify.app/",
+  },
+  {
+    img: "../assets/Battleships.jpg",
+    title: "Battleships",
+    body: "A project about bla bla... bla bla bla bla blabla bla bla bla bla bla bla bla",
+    href: "https://battleships-with-react-client.netlify.app/",
+  },
+  {
+    img: "../assets/The_Movie_Database.jpg",
+    title: "The Movie Database",
+    body: "A project about bla bla... bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla blabla bla bla bla bla bla bla blabla bla bla bla bla bla bla blabla bla bla bla bla bla bla bla",
+    href: "https://frans-gustavson-passe-tmdb-with-react.netlify.app/",
+  },
+  {
+    img: "../assets/SWAPI.jpg",
+    title: "The Star Wars API",
+    body: "A project about bla bla... bla bla bla bla bla bla bla bla bla bla bla ",
+    href: "https://frans-gustavson-passe-swapi-react.netlify.app/",
+  },
+  {
+    img: "../assets/Graphic_tools.jpg",
+    title: "Graphic tooling",
+    body: "A project about bla bla... bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla blabla bla bla bla bla bla bla blabla bla bla bla bla bla bla blabla bla bla bla bla bla bla bla",
+    href: "https://frans-gustavson-passe-grafiska-verktyg.netlify.app/",
+  },
+  {
+    img: "../assets/Guess_the_classmate.jpg",
+    title: "Guess the classmate",
+    body: "A project about bla bla... bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla blabla bla bla bla bla bla bla blabla bla bla bla bla bla bla blabla bla bla bla bla bla bla bla",
+    href: "https://frans-gustavson-passe-student-guessing-name.netlify.app/",
+  },
+];
---
+++ src/types/index.ts
@@ -0,0 +1,7 @@
+export interface Card {
+  href: string;
+  img: string;
+  imgAlt?: string;
+  title: string;
+  body: string;
+}
